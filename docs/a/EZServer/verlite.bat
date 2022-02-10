77f07fffcf18b88bf44a86b86f022a41d752c1b2
wellsilver <wellsilver2016@gmail.com>
2022-02-10 16:46:41 -0500

    Update verlite.bat

--- docs/a/EZServer/verlite.bat
+++ docs/a/EZServer/verlite.bat
@@ -18,19 +18,19 @@ goto error
 ::versions
 
 :1.18
-bitsadmin /transfer "Download Mojang 1.18" /download /priority foreground https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar %APPDATA%/EZServerlite/serv/lol.jar
+echo Downloading-
+powershell -command "wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar -outfile %APPDATA%/EZServerlite/serv/lol.jar"
 goto eof
 
 :P1.18
-bitsadmin /transfer "Download paper 1.18#177" /download /priority foreground https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/177/downloads/paper-1.18.1-177.jar %APPDATA%/EZServerlite/serv/lol.jar
+powershell -command "wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/177/downloads/paper-1.18.1-177.jar -outfile %APPDATA%/EZServerlite/serv/lol.jar"
 goto eof
-
 
 
 
 ::eg
 :error
 cls
-echo This is an invalid version-
+echo This is an invalid version
 pause
 goto home
