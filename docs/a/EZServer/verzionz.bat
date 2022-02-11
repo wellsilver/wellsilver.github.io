c21b1dac92e5fc6d1aa22199c2cd9b0c57801b92
wellsilver <wellsilver2016@gmail.com>
2022-02-11 16:50:51 -0500

    Update verzionz.bat

--- docs/a/EZServer/verzionz.bat
+++ docs/a/EZServer/verzionz.bat
@@ -1,25 +1,71 @@
 @echo off
-:: cool
+:home2
+cls
+:hom2
+echo Type "list" to view available versions.
+echo Type the version you wish to download to download it.
+echo You may also type "anxiety" if you feel any anxiety towards updating to a different build/version
+::LOLLLLL
+set /p w=$ 
+::commands
+if %w%==anxiety goto anxiety
+if %w%==list goto list
 
-:down
-if %h%==1.18 goto 1.18
-if %h%==P1.18 goto P1.18
-goto er
+::versions
+if %w%==P1.18 goto P1.18
+if %w%==P1.17 goto 1.17
+if %w%==1.18 goto 1.18
+if %w%==1.17 goto 1.17
+goto fail
+::commands
+
+:fail
+cls
+echo You didnt select a valid version/command.
+echo.
+goto hom2
+:list
+cls
+echo Currently available versions:
+echo 1.17 (and paper, "P1.17")
+echo 1.18 (and paper, "P1.18")
+echo.
+pause
+goto home2
+
+:anxiety
+echo When upgrading to a more recent version
+echo The next version will automaticly convert the world so dont worry.
+echo.
+echo When converting to a build like paper, it also automaticly
+echo Converts to the paper version.
+echo.
+echo Also all downloads are stable and log4j is patched on all minecraft versions
+pause
+goto home2
+
+::downloads
 
 :1.18
-
-goto end
-:P1.18
-
-goto end
-
-:er
-pause
-echo That isnt a valid version!
-echo Enter valid version:
+cls
+echo Downloading Mojang 1.18,
 echo.
-set /p h=$ 
-goto down
-
-:end
-echo Placeholder
+powershell -command "wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -outfile %APPDATA%/EZServer6/serv/lol.jar"
+goto eof
+:P1.18
+cls
+echo Downloading Paper 1.18 #187,
+echo.
+powershell -command "wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/187/downloads/paper-1.18.1-187.jar -outfile %APPDATA%/EZServer6/serv/lol.jar"
+goto eof
+:1.17
+cls
+echo Downloading Mojang 1.17,
+echo.
+powershell -command "wget https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar -outfile %APPDATA%/EZServer6/serv/lol.jar"
+:P1.17
+cls
+echo Downloading Paper 1.17 #408,
+echo.
+powershell -command "wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/408/downloads/paper-1.17.1-408.jar -outfile %APPDATA%/EZServer6/serv/lol.jar"
+goto eof
